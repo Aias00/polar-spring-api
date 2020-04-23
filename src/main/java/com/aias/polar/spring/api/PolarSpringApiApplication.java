a9cdd9c1c0e1cefe5a83f877001f928ca00d60c3
liuhy <rokkki@163.com>
2020-04-23 14:46:59 +0800

    init

---
+++ src/main/java/com/aias/polar/spring/api/PolarSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.aias.polar.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarSpringApiApplication.class, args);
+    }
+
+}
